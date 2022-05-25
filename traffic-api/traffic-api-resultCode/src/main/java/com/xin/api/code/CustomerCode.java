8a2b931ec7648ad4608fe321709537da016050df
chenx23333@gmail.com <chenx23333@gmail.com>
2022-05-25 12:43:17 -0400

    setup project

---
+++ traffic-api/traffic-api-resultCode/src/main/java/com/xin/api/code/CustomerCode.java
@@ -0,0 +1,10 @@
+package com.xin.api.code;
+
+/**
+ * traffic-customer Customer Module 15556 - 19999
+ * Customer Management Error Code
+ */
+public interface CustomerCode {
+
+    int Customer_ERROR_ADD_FAIL = 15556;
+}
---
+++ traffic-api/traffic-api-resultCode/src/main/java/com/xin/api/code/SystemCode.java
@@ -0,0 +1,24 @@
+package com.xin.api.code;
+
+/**
+ *
+ * System Management Module Error Code
+ * range : 10000 - 15555
+ */
+public interface SystemCode {
+    /**
+     * Error Suggestion Warning
+     */
+    // User Management
+    // 10000 - 10499 Fail Code
+    int SYSTEM_USER_ERROR_ADD_FAIL = 10000;
+
+
+    // 10500 - 10999 Success Code
+    int SYSTEM_USER_INFO_ADD_FAIL = 10500;
+
+    // Role Management 11000 - 11999
+    int SYSTEM_Role_Error_ADD_FAIL = 11000;
+
+    // Permission Management 12000 - 12999
+}
